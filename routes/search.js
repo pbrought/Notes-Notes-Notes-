20c686de3878dd59d40794d0573288bcab8333d3
timm-allman <tallman@umass.edu>
2013-11-04 13:44:36 -0500

    Disable highlighting

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,7 @@
  */
 
 exports.searchAll = function( req, res ) {
-	res.render( 'search All', { title : 'Search All', query : "Query" } );
+	res.render( 'search all', { title : 'Search All', query : "Query" } );
 };
 
 exports.searchNotebook = function( req, res ){
