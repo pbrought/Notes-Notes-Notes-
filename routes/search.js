b204229a90dff78fe974f4344185bc5fc83001c4
timm-allman <tallman@umass.edu>
2013-11-10 20:59:53 -0500

    A whole bunch of stuff Added two helpers Almost finished searching

--- routes/search.js
+++ routes/search.js
@@ -2,10 +2,34 @@
  * Created by tallman on 10/28/13.
  */
 
+var note = require( '../lib/note' );
+var notebook = require( '../lib/notebook' );
+
 exports.searchAll = function( req, res ) {
-	res.render( 'search all', { title : 'Search All', query : "Query" } );
+	var query = req.query.q;
+
+	var noteResults = note.find( query );
+	var notebookResults = notebook.find( query );
+
+	var results = noteResults.concat( notebookResults );
+
+	results.sort( function( a, b ) {
+		if ( a.title == b.title ) {
+			return 0;
+		} else if ( a.title < b.title ) {
+			return -1;
+		} else {
+			return 1;
+		}
+	} );
+
+	res.render( 'search all', {
+		title : 'Search All',
+		query : query,
+		results : results
+	} );
 };
 
-exports.searchNotebook = function( req, res ){
+exports.searchNotebook = function( req, res ) {
 
 };
