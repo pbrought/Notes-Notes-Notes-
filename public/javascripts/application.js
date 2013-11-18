024fff4bbc58bd6b5083bad8dbc35d3869ae8bd6
Timm Allman <tallman@umass.edu>
2013-11-17 21:47:03 -0500

    Start working on display format. For real.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,16 @@
  */
 
 //Responsive Things
-$( function() {
-	var things = $( 'div#nContainer' ).children;
+$( function () {
+	var things = $( 'div#nContainer' ).children,
+		width = window.innerWidth,
+		numThings = things.length,
+		numWide = Math.floor( (width - 140) / 200 ),
+		numTall = Math.ceil( numThings / numWide );
+
+	for ( var i = 0; i < numTall; i++ ) {
+		for ( var j = 0; j < numWide; j++ ) {
+
+		}
+	}
 } );
