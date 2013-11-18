03e51932bfd22cb5a59bbc63cbda7b28067f5be9
Timm Allman <tallman@umass.edu>
2013-11-18 14:44:41 -0500

    Finish initial display format js. Formatting of individual elements still needs work. Refactor out head/foot of all pages into partials

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,15 +4,19 @@
 
 //Responsive Things
 $( function () {
-	var things = $( 'div#nContainer' ).children,
+	var things = $( 'div#nHidden' ).children(),
+		container = $( 'div#nContainer' ),
 		width = window.innerWidth,
 		numThings = things.length,
 		numWide = Math.floor( (width - 140) / 200 ),
 		numTall = Math.ceil( numThings / numWide );
 
 	for ( var i = 0; i < numTall; i++ ) {
-		for ( var j = 0; j < numWide; j++ ) {
-
+		for ( var j = 0; j < numWide && i * numWide + j <= numThings; j++ ) {
+			console.log(i + ',' + j + ',' + ((i * numWide) + j));
+			var thing = $( things[i * numWide + j] );
+			thing.css( {left : j * 200 + 20, top : i * 80 + 10} );
+			container.append( thing );
 		}
 	}
 } );
